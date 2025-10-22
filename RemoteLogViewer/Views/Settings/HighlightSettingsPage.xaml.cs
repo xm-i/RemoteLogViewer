baf469a021c4b7a587454cdd9d067a9ae3eb0918
testakina <testakina@gmail.com>
2025-10-22 23:31:05 +0900

    設定ウィンドウUI修正

--- RemoteLogViewer/Views/Settings/HighlightSettingsPage.xaml.cs
+++ RemoteLogViewer/Views/Settings/HighlightSettingsPage.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace RemoteLogViewer.Views.Settings;
--- RemoteLogViewer/Views/Settings/SettingsWindow.xaml.cs
+++ RemoteLogViewer/Views/Settings/SettingsWindow.xaml.cs
@@ -1,27 +1,25 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Windowing;
 
 namespace RemoteLogViewer.Views.Settings;
 
 public sealed partial class SettingsWindow : Window {
 	public SettingsWindow() {
 		this.InitializeComponent();
-		this.CategoryList.SelectionChanged += CategoryList_SelectionChanged;
-		this.CategoryList.SelectedIndex =0; // default select first
+
+		// ウィンドウサイズを設定
+		this.AppWindow?.Resize(new Windows.Graphics.SizeInt32(1000, 650));
+
+		this.CategoryList.SelectedIndex = 0;
 	}
 
 	private void CategoryList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
 		if (this.CategoryList.SelectedItem is not ListViewItem item) {
 			return;
 		}
-		var key = item.Content as string;
-		switch (key) {
-			case "Highlight":
-				this.ContentFrame.Navigate(typeof(HighlightSettingsPage));
-				break;
-			default:
-				this.ContentFrame.Content = new TextBlock { Text = "Not implemented." };
-				break;
+		if ((item.Content as string) == "Highlight") {
+			this.ContentFrame.Navigate(typeof(HighlightSettingsPage));
 		}
 	}
 }
