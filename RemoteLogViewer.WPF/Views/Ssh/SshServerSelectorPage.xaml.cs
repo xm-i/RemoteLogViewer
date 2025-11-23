05f0a2fa66ddb0623d34fa0662035f571820bafd
testakina <testakina@gmail.com>
2025-11-23 18:42:47 +0900

    WPF化その4

--- RemoteLogViewer.WPF/Views/Ssh/SshServerSelectorPage.xaml.cs
+++ RemoteLogViewer.WPF/Views/Ssh/SshServerSelectorPage.xaml.cs
@@ -1,6 +1,10 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 using Microsoft.Extensions.Logging;
+using Microsoft.Win32;
+
 using RemoteLogViewer.Core.ViewModels.Ssh;
 
 namespace RemoteLogViewer.WPF.Views.Ssh;
@@ -8,12 +12,11 @@ namespace RemoteLogViewer.WPF.Views.Ssh;
 /// <summary>
 /// SSH サーバー選択ページです。接続設定の選択および編集を行います。
 /// </summary>
-public sealed partial class SshServerSelectorPage : Page {
-	private IDisposable? _subscription;
+public sealed partial class SshServerSelectorPage {
 
 	private ILogger<SshServerSelectorPage> logger {
 		get {
-			return field ??= WinUI.App.LoggerFactory.CreateLogger<SshServerSelectorPage>();
+			return field ??= App.LoggerFactory.CreateLogger<SshServerSelectorPage>();
 		}
 	}
 	/// <summary>
@@ -29,56 +32,13 @@ public sealed partial class SshServerSelectorPage : Page {
 	/// </summary>
 	public SshServerSelectorPage() {
 		this.InitializeComponent();
-		this.PasswordBox.PasswordChanged += this.PasswordBox_PasswordChanged;
-		this.PrivateKeyPassphraseBox.PasswordChanged += this.PrivateKeyPassphraseBox_PasswordChanged;
-	}
-
-	private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e) {
-		if (this.ViewModel?.SelectedSshConnectionInfo.Value is { } vm) {
-			vm.Password.Value = this.PasswordBox.Password;
-		}
-	}
-
-	private void PrivateKeyPassphraseBox_PasswordChanged(object sender, RoutedEventArgs e) {
-		if (this.ViewModel?.SelectedSshConnectionInfo.Value is { } vm) {
-			vm.PrivateKeyPassphrase.Value = this.PrivateKeyPassphraseBox.Password;
-		}
-	}
-
-	/// <summary>
-	/// ナビゲート時に ViewModel を受け取ります。
-	/// </summary>
-	protected override void OnNavigatedTo(NavigationEventArgs e) {
-		if (e.Parameter is SshServerSelectorViewModel vm) {
-			this.ViewModel = vm;
-			this._subscription = this.ViewModel.SelectedSshConnectionInfo.ObservePropertyChanged(x => x.Value).Subscribe(this.OnSelectedConnectionChanged);
-		} else {
-			throw new InvalidOperationException("ViewModel is not passed.");
-		}
-		base.OnNavigatedTo(e);
-	}
-
-	protected override void OnNavigatedFrom(NavigationEventArgs e) {
-		this._subscription?.Dispose();
-		this._subscription = null;
-		base.OnNavigatedFrom(e);
-	}
-
-	private void OnSelectedConnectionChanged(SshConnectionInfoViewModel? vm) {
-		if (vm == null) {
-			this.PasswordBox.Password = string.Empty;
-			this.PrivateKeyPassphraseBox.Password = string.Empty;
-		} else {
-			this.PasswordBox.Password = vm.Password.Value ?? string.Empty;
-			this.PrivateKeyPassphraseBox.Password = vm.PrivateKeyPassphrase.Value ?? string.Empty;
-		}
 	}
 
 	/// <summary>
 	/// 保存済み接続一覧ダブルタップ時の処理です。
 	/// </summary>
-	private void SavedConnections_DoubleTapped(object sender, DoubleTappedRoutedEventArgs _) {
-		if(this.ViewModel == null) {
+	private void SavedConnections_MouseDoubleClick(object sender, MouseButtonEventArgs _) {
+		if (this.ViewModel == null) {
 			return;
 		}
 		if (sender is ListBox lb && lb.SelectedItem is SshConnectionInfoViewModel info) {
@@ -101,30 +61,21 @@ public sealed partial class SshServerSelectorPage : Page {
 	/// <summary>
 	/// 秘密鍵ファイル選択ボタン押下時の処理です。
 	/// </summary>
-	private async void BrowsePrivateKeyButton_Click(object sender, RoutedEventArgs e) {
-		if (this.ViewModel == null) {
-			return;
-		}
-		if (this.ViewModel.SelectedSshConnectionInfo.Value is not { } vm) {
+	private void BrowsePrivateKeyButton_Click(object sender, RoutedEventArgs e) {
+		if (this.ViewModel?.SelectedSshConnectionInfo.Value is not { } vm) {
 			return;
 		}
 		try {
-			var picker = new FileOpenPicker {
-				ViewMode = PickerViewMode.List,
-				SuggestedStartLocation = PickerLocationId.DocumentsLibrary,
+			var dialog = new OpenFileDialog {
+				Title = "Select Private Key",
+				Filter = "Key Files|*.pem;*.ppk;*.key;*.pub;*.crt;*.der;*.rsa|All Files|*.*",
+				Multiselect = false
 			};
-			var hwnd = WindowNative.GetWindowHandle(WinUI.App.MainWindow);
-			InitializeWithWindow.Initialize(picker, hwnd);
-			string[] exts = [".pem", ".ppk", ".key", ".pub", ".crt", ".der", ".rsa"];
-			foreach (var ext in exts) {
-				picker.FileTypeFilter.Add(ext);
-			}
-			var file = await picker.PickSingleFileAsync();
-			if (file != null) {
-				vm.PrivateKeyPath.Value = file.Path;
+			if (dialog.ShowDialog() == true) {
+				vm.PrivateKeyPath.Value = dialog.FileName;
 			}
 		} catch (Exception ex) {
-			this.logger.LogError(ex, "Exception");
+			this.logger.LogError(ex, "Exception selecting private key file");
 		}
 	}
 }
