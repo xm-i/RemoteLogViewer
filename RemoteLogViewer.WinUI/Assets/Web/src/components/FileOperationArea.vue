f43833a112703d66a941db03ac73ab467dc32c92
testakina <testakina@gmail.com>
2025-12-08 22:29:24 +0900

    jsの累乗は**

--- RemoteLogViewer.WinUI/Assets/Web/src/components/FileOperationArea.vue
+++ RemoteLogViewer.WinUI/Assets/Web/src/components/FileOperationArea.vue
@@ -115,11 +115,11 @@ const formattedTotalBytes = computed(() => {
 });
 
 const formatBytes = (bytes: number): string => {
-	if (bytes >= (1024 ^ 3)) {
-    return (bytes / (1024 ^ 3)).toFixed(1) + 'GB';
+	if (bytes >= (1024 ** 3)) {
+    return (bytes / (1024 ** 3)).toFixed(1) + 'GB';
 	}
-  if (bytes >= (1024 ^ 2)) {
-    return (bytes / (1024 ^ 2)).toFixed(1) + 'MB';
+  if (bytes >= (1024 ** 2)) {
+    return (bytes / (1024 ** 2)).toFixed(1) + 'MB';
 	}
 	if (bytes >= 1024) {
 		return (bytes / 1024).toFixed(1) + 'KB';
